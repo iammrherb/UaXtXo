fd0b05c29dc5ab1a80eb785c93af074bbbddec3d
iammrherb <iammrherb@gmail.com>
2025-05-18 16:22:49 -0500

    Initla Release - REACT and GH-Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import InitialLoading from './components/ui/InitialLoading';
 import reportWebVitals from './reportWebVitals';
 
-const RootComponent: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate initial loading (can be replaced with actual initialization logic)
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <InitialLoading />;
-  }
-
-  return <App />;
-};
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <RootComponent />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint.
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
