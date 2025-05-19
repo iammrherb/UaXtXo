0ef1d484ce6902a7fc6c8fb5383c4c6dfb420fd1
iammrherb <iammrherb@gmail.com>
2025-05-19 18:02:43 -0500

    Update Portnox TCO Analyzer - Mon May 19 06:02:43 PM CDT 2025

---
+++ precache-manifest.6682ce2f1318327bd351c3db1962d6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6456411c06d08e3630df52b1953a112",
+    "url": "/UaXtXo/index.html"
+  },
+  {
+    "revision": "00d22151b44de2722355",
+    "url": "/UaXtXo/static/css/main.7232817e.chunk.css"
+  },
+  {
+    "revision": "93b86f38ea53fc4dc72b",
+    "url": "/UaXtXo/static/js/2.93e00026.chunk.js"
+  },
+  {
+    "revision": "aec93b037fabce50167b6dfa593ca056",
+    "url": "/UaXtXo/static/js/2.93e00026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f6a1e59d41b04c537e",
+    "url": "/UaXtXo/static/js/3.12bd9ad9.chunk.js"
+  },
+  {
+    "revision": "00d22151b44de2722355",
+    "url": "/UaXtXo/static/js/main.994d79eb.chunk.js"
+  },
+  {
+    "revision": "79d32a93b4e141e742d2",
+    "url": "/UaXtXo/static/js/runtime-main.81a02d2f.js"
+  }
+]);
